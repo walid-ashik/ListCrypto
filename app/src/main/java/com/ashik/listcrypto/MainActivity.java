c864fcec279e25ed1debdc4626b30b0c8ac650c2
walid-ashik <walid.al.ashik@gmail.com>
2018-03-30 00:59:35 +0600

    Fetch All JSON Array (Coin info) from API

--- app/src/main/java/com/ashik/listcrypto/MainActivity.java
+++ app/src/main/java/com/ashik/listcrypto/MainActivity.java
@@ -51,9 +51,20 @@ public class MainActivity extends AppCompatActivity {
 
                         String id = currency.getString("id");
                         String coin_name = currency.getString("name");
+                        String symbol = currency.getString("symbol");
                         String rank = currency.getString("rank");
+                        String price_usd = currency.getString("price_usd");
+                        String price_btc = currency.getString("price_btc");
+                        String volume_usd_24h = currency.getString("24h_volume_usd");
+                        String market_cap_usd = currency.getString("market_cap_usd");
+                        String available_supply = currency.getString("available_supply");
+                        String total_supply = currency.getString("total_supply");
+                        String max_supply = currency.getString("max_supply");
+                        String percent_change_1h = currency.getString("percent_change_1h");
+                        String percent_change_24h = currency.getString("percent_change_24h");
+                        String percent_change_7d = currency.getString("percent_change_7d");
+                        String last_updated = currency.getString("last_updated");
 
-                        Log.d(TAG, "onResponse: " + "\nid: " + id + "\n" + "coin_name: " + coin_name + "\n" + "rank: " + rank + "\n\n");
 
                     } catch (JSONException e) {
                         e.printStackTrace();
